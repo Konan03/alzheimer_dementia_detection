801fb75db7d4ffa277e20422eb5014c145bb012e
Manuel Caicedo <91022649+Konan03@users.noreply.github.com>
2024-11-15 22:14:09 -0500

    fixed: delete more columns inapropied for prediction

--- scripts/make_predictions.py
+++ scripts/make_predictions.py
@@ -1,15 +1,21 @@
-import joblib
 import pandas as pd
+import joblib
 
 # Cargar el modelo entrenado
-model = joblib.load('models/random_forest_model.pkl')
+model = joblib.load('../models/random_forest_model.pkl')
 
 # Cargar los datos de prueba
-test_features_encoded = pd.read_csv('data/test_features_encoded.csv')
+test_features = pd.read_csv('../data/test_features_cleaned.csv')
 
-# Asegurarse de que los datos de prueba no incluyan la columna 'uid' antes de predecir
-test_data = test_features_encoded.drop(columns=['uid'], errors='ignore')
+# Guardar uid por separado
+uid_column = test_features['uid']
+X_test = test_features.drop(columns=['uid'], errors='ignore')
 
-# Realizar predicciones
-predictions = model.predict(test_data)
-print(predictions)
+# Generar predicciones
+predictions = model.predict(X_test)
+
+# Mapear predicciones con uid
+output = pd.DataFrame({'uid': uid_column, 'composite_score': predictions})
+output.to_csv('../data/submission_predictions.csv', index=False)
+
+print("Predicciones guardadas en '../data/submission_predictions.csv'")
